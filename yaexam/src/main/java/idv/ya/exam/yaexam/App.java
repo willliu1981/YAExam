378d53eb1f9eed18fbfb7ad3234e57698f100d89
will.liu <kya9988@yahoo.com.tw>
2022-12-16 13:09:36 +0800

    init

---
+++ yaexam/src/main/java/idv/ya/exam/yaexam/App.java
@@ -0,0 +1,13 @@
+package idv.ya.exam.yaexam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "祐安考題" );
+    }
+}
